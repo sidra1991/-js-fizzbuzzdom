5c0209d9d9048e03f36e74ab3466cce117decfc9
sidra1991 <1991sidra@gmail.com>
2022-02-23 15:05:15 +0100

    base

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// definire le variabili di base - multipli di 3 - multipli di 5 - multipli in comune- codi da inserirle nel circuito + inserire un prompt cosi da lasciare che sia l'utente a richiedere il numero di copie che gli interessano.
+
+// creare circuito con tutte le categorie
+
+alert(" non ho finito (ho appena iniziato) ")
