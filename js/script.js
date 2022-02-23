0a9a2ba72e9a906d07af42168dbac3a27cbd4cab
sidra1991 <1991sidra@gmail.com>
2022-02-23 18:42:57 +0100

    fine o quasi

--- js/script.js
+++ js/script.js
@@ -29,15 +29,15 @@
 // MILESTONE 2
 // Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 
-// for (let number = 1; number <= 100 ; number++) {
+// for (let number = 1; number <= 100; number++) {
 //     if (number % 3 === 0 && number % 5 === 0) {
 //         document.writeln("FizzBuzz")
-//     } else if (number % 5 === 0){
+//     } else if (number % 5 === 0) {
 //         document.writeln("buzz")
-//     } else if (number % 3 === 0 ) {
+//     } else if (number % 3 === 0) {
 //         document.writeln("fizz")
-//     }else {
-//         document.writeln (number)
+//     } else {
+//         document.writeln(number)
 //     }
 // }
 
@@ -48,18 +48,20 @@
 // Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare? Come creare nuovi elementi html e appenderli al container?
 // Consigli del giorno:
 
-for (let number = 1; number <= 100 ; number++) {
+
+let container = document.querySelector(`.container`)
+
+for (let number = 1; number <= 100; number++) {
     if (number % 3 === 0 && number % 5 === 0) {
-        document.writeln("FizzBuzz")
-    } else if (number % 5 === 0){
-        document.writeln("buzz")
-    } else if (number % 3 === 0 ) {
-        document.writeln("fizz")
-    }else {
-        document.writeln (number)
+        container.innerHTML = container.innerHTML +`<div class="square bgFizzBuzz"> Fizz buzz ${number} </div>`
+    } else if (number % 5 === 0) {
+        container.innerHTML = container.innerHTML +`<div class="square bgBuzz"> Buzz ${number}</div>`
+    } else if (number % 3 === 0) {
+        container.innerHTML = container.innerHTML +`<div class="square bgFizz"> Fizz ${number}</div>`
+    } else {
+        container.innerHTML = container.innerHTML +`<div class="square bgNumber ">${number}</div>`
     }
 }
-
 
 
 // 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
@@ -69,4 +71,4 @@ for (let number = 1; number <= 100 ; number++) {
 
 
 
-alert(" non ho finito (sto passando al milestone 3)")
+alert(" finito (sto inserendo scemnze) grazie mauro ")
